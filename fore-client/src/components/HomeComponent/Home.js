caaed9ed306223fdfe0171f50a011d089fd213dc
Danilo Stanojevic <stanojevicdanilo@gmail.com>
2022-06-14 21:20:40 +0200

    fixed homepage bug

--- fore-client/src/components/HomeComponent/Home.js
+++ fore-client/src/components/HomeComponent/Home.js
@@ -14,51 +14,53 @@ function Home() {
             <h2 className='text-center text-light display-6'>to Fore Noćne More</h2>
 
             <Row className='d-flex flex-wrap justify-content-center align-items-center mt-5'>
-                <Col 
-                    className='d-block border rounded border-dark shadow p-5 m-5 bg-light' 
-                >
-                    <blockquote className="blockquote text-center">
-                        <p className="mb-4 display-6">Did you ever wonder how do trees access the internet?</p>
-                        <h4 className='text-muted mb-4'>- They log in ;)</h4>
-                        <small 
-                            className={
-                                    !window.sessionStorage.getItem('token') ? 'd-none' : 'text-muted' 
-                                }
-                            >
-                                So did you.
-                        </small>
-                    </blockquote>
-                </Col>
-
-                <div className='w-100'></div>
-                <Col className=
-                    {
-                        window.sessionStorage.getItem('token') ? 'd-none' : 'd-block border rounded border-dark shadow p-5 m-5 bg-light' 
-                    }
-                >
-                    <blockquote className="blockquote text-center">
-                        <p className='text-center h1'>Don't have account?</p>
-                        <btn 
-                            className='btn btn-outline-success btn-lg m-2' 
-                            onClick={() => setSignUpModalShow(true)}
-                        >Sign Up</btn>
-                        <p className='text-center text-muted'>Make your first joke!</p>
-                    </blockquote>
-                </Col>
-                <Col className=
-                    {
-                        window.sessionStorage.getItem('token') ? 'd-none' : 'd-block border rounded border-dark shadow p-5 m-5 bg-light' 
-                    }
-                >
-                    <blockquote className="blockquote text-center">
-                        <p className='text-center h1'>Already have account?</p>
-                        <btn 
-                            className='btn btn-outline-primary btn-lg m-2'
-                            onClick={() => setLogInModalShow(true)}
-                        >Log In</btn>
-                        <p className='text-center text-muted'>Be the first one to make joke today!</p>
-                    </blockquote>
-                </Col>
+                <Row>
+                    <Col 
+                        className='d-block border rounded border-dark shadow p-5 m-5 bg-light' 
+                        >   
+                        <blockquote className="blockquote text-center">
+                            <p className="mb-4 display-6">Did you ever wonder how do trees access the internet?</p>
+                            <h4 className='text-muted mb-4'>- They log in ;)</h4>
+                            <small 
+                                className={
+                                        !window.sessionStorage.getItem('token') ? 'd-none' : 'text-muted' 
+                                    }
+                                >
+                                    So did you.
+                            </small>
+                        </blockquote>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className=
+                        {
+                            window.sessionStorage.getItem('token') ? 'd-none' : 'd-block border rounded border-dark shadow p-5 m-5 bg-light' 
+                        }
+                    >
+                        <blockquote className="blockquote text-center">
+                            <p className='text-center h1'>Don't have account?</p>
+                            <btn 
+                                className='btn btn-outline-success btn-lg m-2' 
+                                onClick={() => setSignUpModalShow(true)}
+                            >Sign Up</btn>
+                            <p className='text-center text-muted'>Make your first joke!</p>
+                        </blockquote>
+                    </Col>
+                    <Col className=
+                        {
+                            window.sessionStorage.getItem('token') ? 'd-none' : 'd-block border rounded border-dark shadow p-5 m-5 bg-light' 
+                        }
+                    >
+                        <blockquote className="blockquote text-center">
+                            <p className='text-center h1'>Already have account?</p>
+                            <btn 
+                                className='btn btn-outline-primary btn-lg m-2'
+                                onClick={() => setLogInModalShow(true)}
+                            >Log In</btn>
+                            <p className='text-center text-muted'>Be the first one to make joke today!</p>
+                        </blockquote>
+                    </Col>
+                </Row>
             </Row>
 
             <SignUpModal 
