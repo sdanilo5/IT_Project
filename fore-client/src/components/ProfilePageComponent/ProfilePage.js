6c04063973ce451be2ccec92bb630e839575faed
Danilo Stanojevic <stanojevicdanilo@gmail.com>
2022-06-14 17:02:42 +0200

    fixd bugs

--- fore-client/src/components/ProfilePageComponent/ProfilePage.js
+++ fore-client/src/components/ProfilePageComponent/ProfilePage.js
@@ -48,44 +48,44 @@ const ProfilePage = (props) => {
 
     return (
         <>
-            <div class="row py-5 px-4">
-                <div class="col-xl-8 col-md-10 col-sm-10 mx-auto">
+            <div className="row py-5 px-4">
+                <div className="col-xl-8 col-md-10 col-sm-10 mx-auto">
 
-                    <div class="bg-white shadow rounded overflow-hidden">
+                    <div className="bg-white shadow rounded overflow-hidden">
 
-                        <div class="px-4 pt-0 pb-4 bg-dark">
-                            <div class="media align-items-end profile-header">
-                                <div class="profile mr-3">
+                        <div className="px-4 pt-0 pb-4 bg-dark">
+                            <div className="media align-items-end profile-header">
+                                <div className="profile mr-3">
                                     <img 
                                         src={typeof props.img === 'undefined' ? defaultImg : props.img}
                                         alt="..." 
                                         width="35%" 
-                                        class="rounded mb-2 mt-4 img-thumbnail" 
+                                        className="rounded mb-2 mt-4 img-thumbnail" 
                                     />
-                                    <a href="#" class="btn btn-dark btn-sm btn-block">Edit profile</a>
+                                    <a href="#" className="btn btn-dark btn-sm btn-block">Edit profile</a>
                                 </div>
-                                <div class="media-body mb-5 text-white">
-                                    <h4 class="mt-0 mb-0">{user.name}</h4>
-                                    <p class="small mb-4"> 
-                                        <i class="fa fa-map-marker mr-2"></i>
+                                <div className="media-body mb-5 text-white">
+                                    <h4 className="mt-0 mb-0">{user.name}</h4>
+                                    <p className="small mb-4"> 
+                                        <i className="fa fa-map-marker mr-2"></i>
                                         San Farcisco
                                     </p>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="bg-light p-4 d-flex justify-content-end text-center">
+                        <div className="bg-light p-4 d-flex justify-content-end text-center">
                             
-                            <ul class="list-inline mb-0">
-                                <li class="list-inline-item">
-                                    <h5 class="font-weight-bold mb-0 d-block">{user.jokes.length}</h5><small class="text-muted"> <i class="fa fa-picture-o mr-1"></i>Jokes</small>
+                            <ul className="list-inline mb-0">
+                                <li className="list-inline-item">
+                                    <h5 className="font-weight-bold mb-0 d-block">{user.jokes.length}</h5><small className="text-muted"> <i className="fa fa-picture-o mr-1"></i>Jokes</small>
                                 </li>
                             </ul>
                         </div>
 
-                        <div class="py-4 px-4">
-                            <div class="d-flex align-items-center justify-content-between mb-3">
-                                <h5 class="mb-0">{user.name}'s Jokes</h5>
+                        <div className="py-4 px-4">
+                            <div className="d-flex align-items-center justify-content-between mb-3">
+                                <h5 className="mb-0">{user.name}'s Jokes</h5>
                                 <a 
                                     className={ !window.sessionStorage.getItem('token') || (jwt_decode(window.sessionStorage.getItem('token')).id != user.id) ? 'd-none' : 'btn btn-outline-primary'}
                                     onClick={() => setInsertJokeModalShow(true)}
@@ -94,10 +94,10 @@ const ProfilePage = (props) => {
                                 </a>
                             </div>
                             <hr/>
-                            <div class="row flex-grid justify-content-center align-content-center">
+                            <div className="row flex-grid justify-content-center align-content-center">
                                 {
                                     user.jokes.map(joke => {
-                                        return <div class="col d-flex justify-content-center align-content-center">
+                                        return <div className="col d-flex justify-content-center align-content-center">
                                                     <SingleJoke
                                                         joke={joke}
                                                         user={{id: user.id, name: user.name}}
